4dfbf1b0f78eebef99b9c0d7136f3800ec4c619b
luismolina13 <luismol@umich.edu>
2013-11-17 21:45:40 -0500

    Organization of scripts in folders

---
+++ Assets/Scripts/PlayGameScripts/CharacterMovementScripts/Move.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var moveSpeed: float = 1.0;
+var turnSpeed: float = 1.0;
+
+function Update () {
+
+	if(Input.GetButton("W")) {
+		transform.position += transform.forward * moveSpeed * Time.deltaTime;
+		animation.Play("Walk");
+	}
+	else if(Input.GetButton("S")) {
+		transform.position += -transform.forward * moveSpeed * Time.deltaTime;
+		animation.Play("Walk");
+	}
+	else {
+		animation.Play("idle");
+	}
+	
+	if(Input.GetButton("A")) {
+		transform.eulerAngles.y += -turnSpeed * Time.deltaTime;
+	}
+	if(Input.GetButton("D")) {
+		transform.eulerAngles.y += turnSpeed * Time.deltaTime;
+	}
+
+}
