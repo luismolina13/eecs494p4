d44163672ebfacc3b6b91ca43af86cf3e3c5061b
Jason Xie <jasonx@umich.edu>
2013-11-17 23:44:07 -0500

    attack

--- Assets/Scripts/PlayGameScripts/CharacterMovementScripts/Player.cs
+++ Assets/Scripts/PlayGameScripts/CharacterMovementScripts/Player.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class Player : MonoBehaviour 
 {
-	public float moveSpeed = 8.0f;
+	public float moveSpeed = 20.0f;
 	public float deadZone = 0.3f;
+
+	bool attack = false;
+	public float attack_cd = 0.0f;
 
 	// Use this for initialization
 	void Start() 
 	{
 	
 	}
-	
-	// Update is called once per frame
-	void Update() 
+
+	void Movement()
 	{
 		float z_pos = transform.position.z + Input.GetAxis ("L_YAxis_1") * moveSpeed * Time.deltaTime;
 		float x_pos = transform.position.x + Input.GetAxis ("L_XAxis_1") * moveSpeed * Time.deltaTime;
@@ -23,7 +25,30 @@ public class Player : MonoBehaviour
 		{
 			Vector3 temp_vec = new Vector3 (Input.GetAxis("R_XAxis_1") * 1000, 5, Input.GetAxis ("R_YAxis_1") * -1000);
 			transform.LookAt (temp_vec);
-		}		
+		}	
+	}
+
+	// Update is called once per frame
+	void Update() 
+	{	
+		Movement ();
+
+		if (attack) 
+		{
+			attack_cd -= Time.deltaTime;
+			if(attack_cd <= 0)
+			{
+				attack = false;
+				animation.Play ();
+			}
+		}
+
+		if (Input.GetAxis ("TriggersR_1") == 1.0f && !attack) 
+		{
+			animation.Play ("Attack");
+			attack = true;
+			attack_cd = 1.533f;
+		}
 	}
 
 	void OnCollisionEnter(Collision col)
