dd90d79e3b299e01a3ea5d3ae72c31edf4ebd88a
Jason Xie <jasonx@umich.edu>
2013-11-17 21:39:04 -0500

    warriormovement

---
+++ Assets/Scripts/FixCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.rotation = Quaternion.Euler(new Vector3(90,0,0));
+	}
+}
