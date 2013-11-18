4f79803d902c45bc864af61e5cd6693d9ea2e4fd
Jason Xie <jasonx@umich.edu>
2013-11-17 21:53:38 -0500

    playermovement

--- Assets/Scripts/PlayGameScripts/CharacterMovementScripts/Player.cs
+++ Assets/Scripts/PlayGameScripts/CharacterMovementScripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour 
 {
 	public float moveSpeed = 8.0f;
-	public float deadZone = 0.7f;
+	public float deadZone = 0.3f;
 
 	// Use this for initialization
 	void Start() 
@@ -15,19 +15,11 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update() 
 	{
-		float z_pos = transform.position.z;
-		float x_pos = transform.position.x;
-		if (Mathf.Abs(Input.GetAxis ("L_YAxis_1")) > deadZone) 
-		{
-			z_pos += Input.GetAxis ("L_YAxis_1") * moveSpeed * Time.deltaTime;
-		}
-		if (Mathf.Abs (Input.GetAxis ("L_XAxis_1")) > deadZone) 
-		{
-			x_pos += Input.GetAxis ("L_XAxis_1") * moveSpeed * Time.deltaTime;
-		}
+		float z_pos = transform.position.z + Input.GetAxis ("L_YAxis_1") * moveSpeed * Time.deltaTime;
+		float x_pos = transform.position.x + Input.GetAxis ("L_XAxis_1") * moveSpeed * Time.deltaTime;
 		transform.position = new Vector3 (x_pos, 5, z_pos);
 		
-		if(Input.GetAxis("R_XAxis_1") != 0 || Input.GetAxis ("R_YAxis_1") != 0)
+		if(Mathf.Abs (Input.GetAxis("R_XAxis_1")) > deadZone || Mathf.Abs(Input.GetAxis ("R_YAxis_1")) > deadZone)
 		{
 			Vector3 temp_vec = new Vector3 (Input.GetAxis("R_XAxis_1") * 1000, 5, Input.GetAxis ("R_YAxis_1") * -1000);
 			transform.LookAt (temp_vec);
